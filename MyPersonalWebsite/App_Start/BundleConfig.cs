0df8b748023722f1da7182b846ed8dd6dd6263aa
Chinmoy Mohanty <chinmoymohanty85@gmail.com>
2016-04-02 00:06:13 +0530

    First Commit of My personal project created in ASP.NET MVC 5

---
+++ MyPersonalWebsite/App_Start/BundleConfig.cs
@@ -0,0 +1,69 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MyPersonalWebsite
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                         "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-migrate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                       "~/Scripts/jquery.unobtrusive*",
+                       "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/metro").Include(
+                          "~/Scripts/dialog.js",
+                          "~/Scripts/dropdown.js",
+                          "~/Scripts/accordion.js",
+                          "~/Scripts/buttonset.js",
+                          "~/Scripts/carousel.js",
+                          "~/Scripts/input-control.js",
+                          "~/Scripts/pagecontrol.js",
+                          "~/Scripts/pagelist.js",
+                          "~/Scripts/rating.js",
+                          "~/Scripts/slider.js",
+                          "~/Scripts/tile-slider.js",
+                          "~/Scripts/tile-drag.js",
+                          "~/Scripts/calendar.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/metrojs2").Include(
+                        "~/Scripts/MetroJs.lt.min.js",
+                        "~/Scripts/jquery.reveal.js"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            //Commenting out the default styles            
+            //bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                       "~/Content/modern.css",
+                       "~/Content/modern-responsive.css"));
+
+            bundles.Add(new StyleBundle("~/Content/metrocss2").Include(
+                       "~/Content/MetroJs.lt.css",
+                       "~/Content/reveal.css"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                        "~/Content/themes/base/jquery.ui.core.css",
+                        "~/Content/themes/base/jquery.ui.resizable.css",
+                        "~/Content/themes/base/jquery.ui.selectable.css",
+                        "~/Content/themes/base/jquery.ui.accordion.css",
+                        "~/Content/themes/base/jquery.ui.autocomplete.css",
+                        "~/Content/themes/base/jquery.ui.button.css",
+                        "~/Content/themes/base/jquery.ui.dialog.css",
+                        "~/Content/themes/base/jquery.ui.slider.css",
+                        "~/Content/themes/base/jquery.ui.tabs.css",
+                        "~/Content/themes/base/jquery.ui.datepicker.css",
+                        "~/Content/themes/base/jquery.ui.progressbar.css",
+                        "~/Content/themes/base/jquery.ui.theme.css"));
+        }
+    }
+}
---
+++ MyPersonalWebsite/Controllers/MailApiController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Web.Mvc;
+
+namespace MyPersonalWebsite.Controllers
+{
+    public class MailApiController : Controller
+    {
+        [HttpPost]
+        public string PostMail(MailModel mailModel)
+        {
+            try
+            {
+                if (String.IsNullOrWhiteSpace(mailModel.SenderEmail))
+                {
+                    return "email";
+                }
+                MailAddress m = new MailAddress(mailModel.SenderEmail);
+                //return true;
+            }
+            catch (FormatException)
+            {
+                return "email";
+            }
+
+
+            try
+            {
+                MailMessage msg1 = new MailMessage();
+                //msg1.From = new MailAddress("mailme@chinmoymohanty.com", !String.IsNullOrWhiteSpace(senderName) ? senderName : "User");
+                //msg1.To.Add("mailme@chinmoymohanty.com");
+                msg1.From = new MailAddress("chinmoymohanty85@gmail.com", !String.IsNullOrWhiteSpace(mailModel.SenderName) ? mailModel.SenderName : "User");
+                msg1.To.Add("chinmoymohanty85@gmail.com");
+                msg1.Subject = "Feedback from Visitor";
+                msg1.Body = mailModel.MailBody;
+                msg1.IsBodyHtml = false;
+
+
+                MailMessage msg2 = new MailMessage();
+                msg2.From = new MailAddress("chinmoymohanty85@gmail.com", "Chinmoy Mohanty");
+                msg2.To.Add(mailModel.SenderEmail);
+                msg2.Subject = "Thank you for your Feedback";
+                msg2.Body = "Hi " + (!String.IsNullOrWhiteSpace(mailModel.SenderName) ? mailModel.SenderName : "User") + "," + Environment.NewLine + Environment.NewLine + "I am very glad that you took the time to provide me feedback and suggestions regarding my website and its deeply appreciated. I'll try to get back to you at the earliest regarding the same." + Environment.NewLine + Environment.NewLine + "Regards," + Environment.NewLine + "Chinmoy Mohanty";
+                msg2.IsBodyHtml = false;
+
+                // Init SmtpClient and send
+                SmtpClient smtpClient = new SmtpClient("smtp.sendgrid.net", Convert.ToInt32(587));
+                System.Net.NetworkCredential credentials = new System.Net.NetworkCredential("chinmoymohanty85", "security@X85s");
+                smtpClient.Credentials = credentials;
+
+                smtpClient.Send(msg1);
+                smtpClient.Send(msg2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return "Success";
+        }
+    }
+
+    public class MailModel
+    {
+        public string SenderName { get; set; }
+        public string SenderEmail { get; set; }
+        public string MailBody { get; set; }
+
+    }
+}
